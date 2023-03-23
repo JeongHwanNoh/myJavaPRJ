9b9396c02d613b6dd4315276901da099084bbbe4
JeongHwanNoh <jeonghwan123456789@gmail.com>
2023-03-23 12:27:49 +0900

    연습

--- src/main/java/bigdata01.java
+++ src/main/java/bigdata01.java
@@ -1,17 +1,22 @@
 import java.util.Scanner;
 
-public class bigdata01 {public static void main(String[] args)
-    {
-            Scanner s = new Scanner(System.in);
-            int a;
+public class bigdata01 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a;
 
-            System.out.printf("3의 배수를 입력하세요 : ");
-            a = s.nextInt();
+        System.out.printf("입력값 : ");
+        a = s.nextInt();
+        System.out.printf("출력결과 \n");
 
-            if (a%3 == 0) {
-                System.out.printf("3의 배수입니다.\n");
-            } else {
-                System.out.printf("3의 배수가 아닙니다.\n");
-            }
+        if (a % 15 == 0) {
+            System.out.printf("3의 배수와 5의 배수를 둘다 만족합니다.\n");
+        } else if (a % 5 == 0) {
+            System.out.printf("5의 배수입니다.\n");
+        } else if (a % 3 == 0) {
+            System.out.printf("3의 배수입니다.\n");}
+            else {System.out.printf("5의 배수도 3의 배수도 아닙니다.\n");
+
         }
     }
+}
