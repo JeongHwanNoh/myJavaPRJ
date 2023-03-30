1b730c0f06b340b69efabc2d75bed1a0e0dc944c
JeongHwanNoh <jeonghwan123456789@gmail.com>
2023-03-30 12:21:11 +0900

    연습

--- src/main/java/bigdata04.java
+++ src/main/java/bigdata04.java
@@ -4,7 +4,7 @@ public class bigdata04 {
 
         for (b = 1; b < 10; b++) {
             for (a = 2; a < 10; a++)
-                System.out.printf("%5dX%d=%2d", a, b, a * b);
+                System.out.printf("%4dX%d=%d", a, b, a * b);
             System.out.printf("\n");
         }
     }
---
+++ src/main/java/bigdata05.java
@@ -0,0 +1,14 @@
+public class bigdata05 {
+    public static void main(String[] args) {
+        int a, b;
+
+        for (a = 2, b = 1; a < 10; b++) {
+            if (b > 9) {
+                b = 0;
+                a++;
+            } else {
+                System.out.printf("%d X %d = %d \n", a, b, a * b);
+            }
+        }
+    }
+}
