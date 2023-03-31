5607c1e57421e8149f6840cba77e6272a45fe445
JeongHwanNoh <jeonghwan123456789@gmail.com>
2023-03-31 14:00:33 +0900

    연습

--- src/main/java/bigdata06.java
+++ src/main/java/bigdata06.java
@@ -1,9 +1,15 @@
-public class bigdata06 { public static void main(String[] args) {
-    int a, b = 0;
-    for (a = 1; a < 6;) {
-        if (b < a) { b++;
-            System.out.print ("*");
-        } else { b = 0; a++; System.out.println ("");}
+public class bigdata06 {
+    public static void main(String[] args) {
+        int a, b = 0;
+        for (a = 1; a < 6; ) {
+            if (b < a) {
+                b++;
+                System.out.print("*");
+            } else {
+                b = 0;
+                a++;
+                System.out.println();
+            }
+        }
     }
-}
 }
--- src/main/java/bigdata07.java
+++ src/main/java/bigdata07.java
@@ -1,10 +1,16 @@
-public class bigdata07 { public static void main(String[] args) {
-    int a, b = 0;
-    for (a = 5; a > 0;) {
-        if (b < a) { b++;
-            System.out.print ("*");
-        } else { b = 0; a--; System.out.println ("");}
+public class bigdata07 {
+    public static void main(String[] args) {
+        int a, b = 0;
+        for (a = 5; a > 0; ) {
+            if (b < a) {
+                b++;
+                System.out.print("*");
+            } else {
+                b = 0;
+                a--;
+                System.out.println();
+            }
+        }
     }
-}
 }
 
