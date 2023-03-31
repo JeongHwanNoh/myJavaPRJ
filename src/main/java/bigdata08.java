e17a0d8a97f23dd33fea4c41a7cfe2d7d484aea8
JeongHwanNoh <jeonghwan123456789@gmail.com>
2023-03-31 16:22:01 +0900

    연습

--- src/main/java/bigdata08.java
+++ src/main/java/bigdata08.java
@@ -1,2 +1,19 @@
 public class bigdata08 {
+    public static void main(String[] args) {
+        int a, b = 0, c = 10;
+        for (a = 1; a < 10; ) {
+            if (a > b && a < 6) {
+                b++;
+                System.out.print("*");
+            } else if (a < c && a > 5) {
+                c--;
+                System.out.print("*");
+            } else {
+                a++;
+                b = 0;
+                c = 10;
+                System.out.println();
+            }
+        }
+    }
 }
