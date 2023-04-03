723c07adfe26c5c8e2321323d09d334f5d3ad1d3
JeongHwanNoh <jeonghwan123456789@gmail.com>
2023-04-03 17:06:08 +0900

    연습

---
+++ src/main/java/bigdata09.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class bigdata09 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a, b;
+        System.out.printf("20 이하로 원하는 값을 입력하세요. : ");
+        a = sc.nextInt();
+
+        if (a > 20) {
+            System.out.printf("수가 20를 초과하셨습니다.\n");
+        } else if (a < 21) {
+            System.out.printf("%d의 값을 입력하셨습니다.\n", a);
+            for (b = 0; a > 0; ) {
+                if (a > b) {
+                    b++;
+                    System.out.print("*");
+                } else {
+                    a--;
+                    b = 0;
+                    System.out.println();
+                }
+
+            }
+        }
+    }
+}
+
+
+
