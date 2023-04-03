dd5d82c2c266d4dfb6055b5845fdab820d6c26a7
JeongHwanNoh <jeonghwan123456789@gmail.com>
2023-04-03 15:27:04 +0900

    연습

--- src/main/java/bigdata05.java
+++ src/main/java/bigdata05.java
@@ -1,13 +1,13 @@
 public class bigdata05 {
     public static void main(String[] args) {
-        int a, b;
+        int a, b; //변수설정
 
-        for (a = 2 , b = 1; a < 10; b++) {
-            if (b > 9) {
-                b = 0;
-                a++;
-            } else {
-                System.out.printf("%d X %d = %d \n", a, b, a * b);
+        for (a = 1, b= 2; b < 10; a++) { //a에 1대입 b에는 2대입. b 보다 작을 경우 실행. 실행되고 난 후 증감식
+            if (a > 9) { //만약 a가 9를 초과하였을경우
+                a = 0; //a에 0 대입
+                b++; // b 증감식
+            } else { //a의 조건이 안될경우
+                System.out.printf("%d X %d = %d \n", b, a, a * b); // b * a 를 실행한다.
             }
         }
     }
